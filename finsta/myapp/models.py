2d8bc91e4407fb94f6186a72424fb31d0c4c5b8c
Bhagyalakshmibhagya <bhagyalakshmik2002@gmail.com>
2023-05-31 07:49:11 +0300

    socialcommit

--- finsta/myapp/models.py
+++ finsta/myapp/models.py
@@ -1,18 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from random import sample
 
-# Create your models here.
 class UserProfile(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE,related_name="profile")
-    profile_pic=models.ImageField(upload_to="profilepics",null=True,blank=True)
-    bio=models.CharField(max_length=200)
-    address=models.CharField(max_length=200)
+    profile_pic=models.ImageField(upload_to="profile_pic",default="profilepics/default.jpg")
+    bio=models.CharField(max_length=200,null=True)
+    address=models.CharField(max_length=200,null=True)
     dob=models.DateTimeField(null=True)
-    following=models.ManyToManyField("self",related_name="followed_by")
+    following=models.ManyToManyField("self",related_name="followed_by",symmetrical=False)
     created_date=models.DateTimeField(auto_now_add=True)
+    cover_pic=models.ImageField(upload_to="coverpic",blank=True,default="/profilepics/cover.jpg")
 
     def __str__(self):
         return self.user.username
+    @property
+    def friend_requests(self):
+        all_profiles=UserProfile.objects.all().exclude(user=self.user)
+        following_profiles=self.following.all()
+        suggestions=set(all_profiles) - set(following_profiles)
+        if len(suggestions) >2:
+            return sample(suggestions,2)
+        return suggestions
+
+
     
 class Posts(models.Model):
     title=models.CharField(max_length=200)
@@ -21,14 +33,22 @@ class Posts(models.Model):
     created_date=models.DateTimeField(auto_now_add=True)
     liked_by=models.ManyToManyField(User,related_name="post_like")
 
+    
     def __str__(self):
         return self.title
-
+    
 class Comments(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE,related_name="comment")
     comment_text=models.CharField(max_length=200)
     post=models.ForeignKey(Posts,on_delete=models.CASCADE,related_name="post_comment")
     created_date=models.DateTimeField(auto_now_add=True)
+    
 
+    
     def __str__(self):
         return self.comment_text
+    
+def CreateProfile(sender,instance,created,**kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+post_save.connect(CreateProfile,sender=User)
